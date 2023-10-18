697f7d3765fd7f49f6056eccab585ba3fadfb38e
vizssouza <vini.nelso@gmail.com>
2023-10-17 21:28:45 -0300

    Atualizações do Protótipo
    Adicionando página de ajuda, arrumando logos na Navbar, adicionando transições nas páginas e adicionou scroll.

--- Projeto Startup/Js/scripts.js
+++ Projeto Startup/Js/scripts.js
@@ -1,14 +1,1 @@
-document.querySelectorAll('.scroll-link').forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        e.preventDefault(); // Impede o comportamento padrão do link
 
-        var targetId = this.getAttribute('href').substr(1); // Pega o ID do link
-        var targetElement = document.getElementById(targetId); // Encontra o elemento alvo
-
-        if (targetElement) {
-        targetElement.scrollIntoView({
-            behavior: 'smooth', // Ativa a animação suave
-        });
-        }
-    });
-});
