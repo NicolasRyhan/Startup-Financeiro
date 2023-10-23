8307db7e849e9a7f860fbdebabce9c1fb8e4c854
vizssouza <vini.nelso@gmail.com>
2023-10-22 23:17:31 -0300

    Atualizando TODAS as Páginas
    Atualizando TODOS os styles de todas as páginas faltando apenas atualizar as fotos da página do sobre e adicionar as medias querys.

--- Projeto Startup/Js/pizza.js
+++ Projeto Startup/Js/pizza.js
@@ -1,31 +1,22 @@
-// Data retrieved from https://gs.statcounter.com/vendor-market-share/mobile/
-Highcharts.chart('container', {
-    chart: {
-        styledMode: true
-    },
-    title: {
-        text: 'Porcentagem de Gastos'
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    series: [{
-        type: 'pie',
-        allowPointSelect: true,
-        keys: ['name', 'y', 'selected', 'sliced'],
-        data: [
-            ['Samsung', 27.79, true, true],
-            ['Apple', 27.34, false],
-            ['Xiaomi', 10.87, false],
-            ['Huawei', 8.48, false],
-            ['Oppo', 5.38, false],
-            ['Vivo', 4.17, false],
-            ['Realme', 2.57, false],
-            ['Unknown', 2.45, false],
-            ['Motorola', 2.22, false],
-            ['LG', 1.53, false],
-            ['Other', 7.2, false]
-        ],
-        showInLegend: true
-    }]
-});
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work',     11],
+            ['Eat',      2],
+            ['Commute',  2],
+            ['Watch TV', 2],
+            ['Sleep',    7]
+        ]);
+
+        var options = {
+            title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+        }
